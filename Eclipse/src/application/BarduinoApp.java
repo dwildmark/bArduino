478614c0f785dbcedb12768fa08b94b6e5a5bc3b
Jonathan <jonathan.e.m.bocker@gmail.com>
2015-04-08 18:29:23 +0200

    Safety Commit
    Som backup, fast commit

--- Eclipse/src/application/BarduinoApp.java
+++ Eclipse/src/application/BarduinoApp.java
@@ -5,10 +5,19 @@ import java.io.IOException;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 public class BarduinoApp {
 	public static void main(String[] args) throws IOException {
+		try {
+			UIManager.setLookAndFeel(
+			        UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {			
+			e.printStackTrace();
+		} 
 		GUI gui = new GUI("src/application/Barduino.png");
+		gui.setIngredients(new String[]{"Vodka","Juice,Orange"});
 		JFrame frame = new JFrame();
 		String absolutePath = new File("src/application/bArduino_icon.png").getAbsolutePath();
 		frame.setIconImage(new ImageIcon(absolutePath).getImage());
--- Eclipse/src/application/GUI.java
+++ Eclipse/src/application/GUI.java
@@ -2,28 +2,33 @@ package application;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.border.Border;
 
 import org.imgscalr.Scalr;
 
 public class GUI extends JPanel {
 	private JLabel lblLogo, glassSizeLbl1, glassSizeLbl2, lblNoConnection;
 	private JPanel drinkPanel, sliderPanel, optionsPanel;
-	private ArrayList<JSlider> sliders;
-	private ArrayList<JLabel> sliderLbls;
+	private ArrayList<JSlider> sliders = new ArrayList<JSlider>();
+	private ArrayList<JLabel> sliderLbls = new ArrayList<JLabel>();
 	private JButton orderBtn, arrowUp, arrowDown;
 	private int glassSize;
+	
 	public GUI( String relPath) throws IOException {
 		File absolutePath = new File(relPath);
 		BufferedImage image = ImageIO.read(absolutePath);
@@ -33,8 +38,6 @@ public class GUI extends JPanel {
 		lblNoConnection = new JLabel("No connection!");
 		lblNoConnection.setAlignmentX(Component.CENTER_ALIGNMENT);
 		lblNoConnection.setVisible(false);
-		glassSizeLbl1 = new JLabel("Drink Size");
-		glassSizeLbl1.setAlignmentX(Component.CENTER_ALIGNMENT);
 		glassSizeLbl2 = new JLabel("25");
 		glassSizeLbl2.setAlignmentX(Component.CENTER_ALIGNMENT);
 		arrowUp = new JButton("More");
@@ -46,26 +49,53 @@ public class GUI extends JPanel {
 		//OptionsPanel
 		optionsPanel = new JPanel();		
 		optionsPanel.setLayout(new BoxLayout(optionsPanel, BoxLayout.Y_AXIS));
-		optionsPanel.add(glassSizeLbl1);
+		optionsPanel.add(Box.createVerticalGlue());
 		optionsPanel.add(arrowUp);
 		optionsPanel.add(glassSizeLbl2);
 		optionsPanel.add(arrowDown);
+		optionsPanel.add(Box.createVerticalGlue());
+		optionsPanel.setBorder(BorderFactory.createTitledBorder(null, "Size [ CL ]"));
 		//sliderPanel
 		sliderPanel = new JPanel();
 		sliderPanel.setLayout(new BoxLayout(sliderPanel, BoxLayout.Y_AXIS));
 		JLabel initLabel = new JLabel("Nothing here yet");
 		initLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
 		sliderPanel.add(initLabel);
+		sliderPanel.setBorder(BorderFactory.createTitledBorder("Choose your drink"));
 		//drinkPanel
 		drinkPanel = new JPanel(new BorderLayout());
 		drinkPanel.add(sliderPanel, BorderLayout.CENTER);
 		drinkPanel.add(optionsPanel, BorderLayout.EAST);
 		//main panel
 		setLayout( new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		add(lblLogo);
 		add(lblNoConnection);
 		add(drinkPanel);
+		add(Box.createRigidArea(new Dimension(0, 5)));
 		add(orderBtn);
+	}
+	
+	public void setIngredients(String[] ingredients){
+		JLabel tempLabel;
+		JSlider tempSlider;
+		
+		sliderPanel.removeAll();
+		
+		for(int i = 0; i < ingredients.length; i++){
+			tempLabel = new JLabel(ingredients[i]);
+			tempLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+			sliderLbls.add(tempLabel);
+			sliderPanel.add(tempLabel);
+			
+			tempSlider = new JSlider(0, Integer.parseInt(glassSizeLbl2.getText()));
+			tempSlider.setAlignmentX(Component.CENTER_ALIGNMENT);
+			sliders.add(tempSlider);
+			sliderPanel.add(tempSlider);
+			
+			sliderPanel.add(Box.createRigidArea(new Dimension(0, 10)));
+			
+		}
 	}
 	
 }
