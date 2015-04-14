85d3d67f84e4c57f8465497d1ea750d3192eb80b
Schwusch <jonathan.e.m.bocker@gmail.com>
2015-04-14 02:15:13 +0200

    Server GUI fix, nu med MigLayout
    Lite externa bibliotek och snyggare serverGUI

--- Eclipse/src/server/ServerGUI.java
+++ Eclipse/src/server/ServerGUI.java
@@ -1,18 +1,13 @@
 package server;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.concurrent.FutureTask;
 import java.util.logging.Logger;
 
 import javax.swing.BorderFactory;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,24 +19,23 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.UIManager;
 
+import net.miginfocom.swing.*;
 
 public class ServerGUI extends JFrame {
 
 	private static final long serialVersionUID = 2486865764551934155L;
 	private JTextField tfFluid1, tfFluid2, tfFluid3, tfFluid4, tfPortClient,
-	tfPortArduino;
+			tfPortArduino;
 	private JTextArea taLog;
 	private JLabel lblFluid1, lblFluid2, lblFluid3, lblFluid4, lblPortClient,
-	lblPortArduino;
+			lblPortArduino;
 	private JButton btnRestart, btnSave, btnQuit;
-	private JPanel pnlNetwork, pnlNetworkLbls, pnlNetworkTfs, pnlFluids,
-	pnlButtons, pnlStatus, pnlMain, pnlFluid1, pnlFluid2, pnlFluid3,
-	pnlFluid4;
+	private JPanel pnlNetwork, pnlFluids,
+			pnlButtons, pnlStatus, pnlMain;
 	private JTabbedPane tabbedPane;
 	private JScrollPane logScrollPane;
 	private Logger logger;
 	private Server server;
-	private FutureTask futureTask;
 
 	public ServerGUI(Logger logger) {
 		this.logger = logger;
@@ -50,11 +44,10 @@ public class ServerGUI extends JFrame {
 		taLog = tah.getTextArea();
 
 		try {
-			UIManager.setLookAndFeel(
-					UIManager.getSystemLookAndFeelClassName());
-		} catch (Exception e) {			
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 
 		Properties prop = null;
 		try {
@@ -69,19 +62,18 @@ public class ServerGUI extends JFrame {
 
 		}
 
+		MigLayout buttonLayout = new MigLayout();
 		// Button Panel
 		btnSave = new JButton("Save");
 		btnRestart = new JButton("Restart");
 		btnQuit = new JButton("Quit");
 
 		pnlButtons = new JPanel();
-		pnlButtons.setLayout(new BoxLayout(pnlButtons, BoxLayout.X_AXIS));
+		pnlButtons.setLayout(buttonLayout);
 		pnlButtons.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		pnlButtons.add(Box.createHorizontalGlue());
 		pnlButtons.add(btnSave);
 		pnlButtons.add(btnRestart);
 		pnlButtons.add(btnQuit);
-		pnlButtons.add(Box.createHorizontalGlue());
 
 		// Fluids Panel
 		tfFluid1 = new JTextField(prop.getProperty("fluid1"));
@@ -94,53 +86,33 @@ public class ServerGUI extends JFrame {
 		lblFluid3 = new JLabel("Fluid 3");
 		lblFluid4 = new JLabel("Fluid 4");
 
-		pnlFluid1 = new JPanel(new BorderLayout());
-		pnlFluid1.add(lblFluid1, BorderLayout.WEST);
-		pnlFluid1.add(tfFluid1, BorderLayout.CENTER);
-
-		pnlFluid2 = new JPanel(new BorderLayout());
-		pnlFluid2.add(lblFluid2, BorderLayout.WEST);
-		pnlFluid2.add(tfFluid2, BorderLayout.CENTER);
-
-		pnlFluid3 = new JPanel(new BorderLayout());
-		pnlFluid3.add(lblFluid3, BorderLayout.WEST);
-		pnlFluid3.add(tfFluid3, BorderLayout.CENTER);
-
-		pnlFluid4 = new JPanel(new BorderLayout());
-		pnlFluid4.add(lblFluid4, BorderLayout.WEST);
-		pnlFluid4.add(tfFluid4, BorderLayout.CENTER);
-
-		pnlFluids = new JPanel();
-		pnlFluids.setLayout(new BoxLayout(pnlFluids, BoxLayout.PAGE_AXIS));
-		pnlFluids.add(pnlFluid1);
-		pnlFluids.add(pnlFluid2);
-		pnlFluids.add(pnlFluid3);
-		pnlFluids.add(pnlFluid4);
-		pnlFluids.add(Box.createVerticalGlue());
+		pnlFluids = new JPanel(new MigLayout());
+		pnlFluids.add(lblFluid1);
+		pnlFluids.add(tfFluid1, "grow, wrap, width 50:150:200");
+		pnlFluids.add(lblFluid2);
+		pnlFluids.add(tfFluid2, "grow, wrap");
+		pnlFluids.add(lblFluid3);
+		pnlFluids.add(tfFluid3, "grow, wrap");
+		pnlFluids.add(lblFluid4);
+		pnlFluids.add(tfFluid4, "grow, wrap");
 
 		// Network Panel
 		lblPortClient = new JLabel("Client Port");
 		lblPortArduino = new JLabel("Arduino Port");
-
-		pnlNetworkLbls = new JPanel(new GridLayout(2, 0));
-		pnlNetworkLbls.add(lblPortClient);
-		pnlNetworkLbls.add(lblPortArduino);
-
 		tfPortClient = new JTextField(prop.getProperty("clientport"));
 		tfPortArduino = new JTextField(prop.getProperty("arduinoport"));
 
-		pnlNetworkTfs = new JPanel(new GridLayout(2, 0));
-		pnlNetworkTfs.add(tfPortClient);
-		pnlNetworkTfs.add(tfPortArduino);
-
-		pnlNetwork = new JPanel(new BorderLayout());
-		pnlNetwork.add(pnlNetworkLbls, BorderLayout.WEST);
-		pnlNetwork.add(pnlNetworkTfs, BorderLayout.CENTER);
+		pnlNetwork = new JPanel(new MigLayout());
+		pnlNetwork.add(lblPortClient);
+		pnlNetwork.add(tfPortClient, "wrap, w 100!");
+		pnlNetwork.add(lblPortArduino);
+		pnlNetwork.add(tfPortArduino, "wrap, w 100!");
 
 		// Status Panel
 		logScrollPane = new JScrollPane(taLog);
-		pnlStatus = new JPanel(new BorderLayout());
-		pnlStatus.add(logScrollPane, BorderLayout.CENTER);
+		logScrollPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+		pnlStatus = new JPanel();
+		pnlStatus.add(logScrollPane);
 
 		// Tabbed Pane
 		tabbedPane = new JTabbedPane();
@@ -148,20 +120,20 @@ public class ServerGUI extends JFrame {
 		tabbedPane.add("Status", logScrollPane);
 		tabbedPane.add("Fluids", pnlFluids);
 		tabbedPane.add("Network", pnlNetwork);
-		
 
 		// Main Panel
-		pnlMain = new JPanel(new BorderLayout());
-		pnlMain.add(pnlButtons, BorderLayout.NORTH);
-		pnlMain.add(tabbedPane, BorderLayout.CENTER);
+		pnlMain = new JPanel(new MigLayout());
+		pnlMain.add(pnlButtons, "wrap");
+		pnlMain.add(tabbedPane, "grow, span, push");
 
-		add(pnlMain);
+		setLayout(new MigLayout());
+		add(pnlMain, "grow, span, push");
 		setTitle("Barduino Server");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		pack();
-		setVisible(true);	
+		setVisible(true);
 
-		//actionlistners
+		// actionlistners
 		Listener btnlistner = new Listener();
 		btnQuit.addActionListener(btnlistner);
 		btnRestart.addActionListener(btnlistner);
@@ -170,31 +142,32 @@ public class ServerGUI extends JFrame {
 		startServer();
 	}
 
-	private void startServer(){
+	private void startServer() {
 		server = new Server(this.logger);
 		server.start();
-		
+
 	}
 
-	public class Listener implements ActionListener{
+	public class Listener implements ActionListener {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource()==btnRestart){
+			if (e.getSource() == btnRestart) {
 				server.close();
 				startServer();
 				logger.info("Server is restarted");
-			}else if (e.getSource()==btnQuit){
-				System.exit(0);			
-			}else if(e.getSource()==btnSave){
-				JOptionPane.showConfirmDialog(btnSave, "Are you sure that you want to save?"
-						+ " Old settings will be lost!");
-				
+			} else if (e.getSource() == btnQuit) {
+				System.exit(0);
+			} else if (e.getSource() == btnSave) {
+				JOptionPane.showConfirmDialog(btnSave,
+						"Are you sure that you want to save?"
+								+ " Old settings will be lost!");
+
 				Properties prop = new Properties();
 				prop = new Properties();
 				String propFileName = "./resources/config.properties";
 				FileOutputStream out;
-				
+
 				try {
 					out = new FileOutputStream(propFileName);
 					prop.setProperty("fluid1", tfFluid1.getText());
@@ -208,9 +181,8 @@ public class ServerGUI extends JFrame {
 				} catch (Exception a) {
 					a.printStackTrace();
 				}
-				
 
-			}		
+			}
 		}
 	}
 }
