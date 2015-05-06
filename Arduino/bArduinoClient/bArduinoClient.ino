3db805348db4de5c7cbc239c8765371f290a6f62
Dennis <d.wildmark@gmail.com>
2015-05-06 13:33:04 +0200

    Ny stabil version med autodiscover.

--- Arduino/bArduinoClient/bArduinoClient.ino
+++ Arduino/bArduinoClient/bArduinoClient.ino
@@ -1,22 +1,41 @@
+/*
+  This is the main program for the Barduino machine. 
+  It discovers a Barduino-server on the network using UDP-broadcast, 
+  and connects to it. When connected it recieves messages from
+  the server and processes the message.
+  The program controls 4 flowmeters and valves. 
+  It uses these to pour drinks based on the information it recieves
+  from the server.
+  
+  @author Dennis Wildmark
+*/
+
 #include <SPI.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
 
 //Enter the MAC-address for the bArduino below.
 byte mac[] = {
   0xDE, 0xAD, 0xBE, 0xEF, 0xDD, 0xAC
 };
 
-//Enter the server IP address below.
-IPAddress server(192, 168, 1, 62);
+IPAddress remote;
+
+unsigned int tcpPort = 8008;
+
+unsigned int udpPort = 28780;
 
 EthernetClient client;
+
+EthernetUDP udp;
+
+char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
 //Define the pins for the fluids
 int liquid1 = 9;
 int liquid2 = 8;
 int liquid3 = 7;
 int liquid4 = 6;
-int state = LOW;
 
 volatile int pulses = 0;
 volatile int chosen_liquid;
@@ -24,21 +43,14 @@ volatile int chosen_liquid;
 void setup()
 {
   Ethernet.begin(mac);
-  
+
+  udp.begin(udpPort);
+
   //give the Ethernet shield a second to initialize:
   delay(1000);
   
-  if (client.connect(server, 8008)) {
-    //Serial.println("connected");
-  }
-  else {
-    // if you didn't get a connection to the server,
-    // keep trying to connect
-    while (!client.connected()) {
-      client.stop();
-      client.connect(server, 8008);
-    }
-  }
+  //look for and connect to a server
+  discoverServer();
 
   //initialize the output pins
   pinMode(liquid1, OUTPUT);
@@ -61,29 +73,29 @@ void loop()
       }
       //Choose what liquid to pour based on the first char
       chooseLiquid(c[0]);
-      
+
       //If the first char is 'Q', respond with 'OK'
       if ((char)c[0] == 'Q') {
         client.print("OK\n");
       } else if (chosen_liquid > 0) {
+        
         //Converts the two numbers into a single integer
         int amount = 10 * ((int)c[1] - 48) + ((int)c[2] - 48);
         pourDrink(chosen_liquid, amount);
         delay(100);
+        
         //Report back to server when done
         client.print("ACK\n");
       } else {
         client.print("BADFORMAT\n");
       }
     }
+    
     //If Arduino disconnects from server
-    //keep try to reconnect
+    //try to find a server again.
     if (!client.connected()) {
       client.stop();
-      while (!client.connected()) {
-        client.stop();
-        client.connect(server, 8008);
-      }
+      discoverServer();
     }
   }
 }
@@ -123,13 +135,35 @@ void chooseLiquid(char liquid) {
 }
 
 int determineAmount(int centiliters) {
-  switch(centiliters) {
+  switch (centiliters) {
     case 1:
       return 2;
       break;
     case 2:
       return 4;
       break;
+  }
+}
+
+void discoverServer() {
+  boolean a = true;
+  while (a) {
+    //IPAddress broadcastIP(255, 255, 255, 255);
+
+    //udp.beginPacket(broadcastIP, udpPort);
+    //udp.write("DISCOVER_FUIFSERVER_REQUEST");
+    //udp.endPacket();
+
+    int packetSize = udp.parsePacket();
+    if (packetSize) {
+      remote = udp.remoteIP();
+      udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+      while (!client.connected()) {
+        client.connect(remote, tcpPort);
+      }
+      a = false;
+    }
+    //delay(1000);
   }
 }
 
