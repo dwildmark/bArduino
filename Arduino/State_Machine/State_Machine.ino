e2b3eea04239b64a8775c6f072737b0d1d209321
Dennis <d.wildmark@gmail.com>
2015-06-04 15:52:04 +0200

    Slutlig uppdatering

--- Arduino/State_Machine/State_Machine.ino
+++ Arduino/State_Machine/State_Machine.ino
@@ -1,41 +1,56 @@
+/*
+  This is the main program for the Barduino machine.
+  It discovers a Barduino-server on the network by listening to UDP-broadcasts,
+  and connects to it. When connected it receives messages from
+  the server and processes the message.
+  The program controls 4 flowmeters and valves, and one LED.
+  It uses these to pour drinks based on the information it receives
+  from the server.
+
+  Contributers: Dennis Wildmark, Jonathan Bocker
+
+  @author Dennis Wildmark
+*/
+
 #include <SPI.h>
 #include <Ethernet.h>
 #include <EthernetUdp.h>
 
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xDD, 0xAF
-};
-
-
 IPAddress remote;
 
-unsigned int tcpPort = 8008;
+//define the tcp port to use
+#define PORT_TCP (8008)
 
-unsigned int udpPort = 28780;
+//define the udp port to use
+#define PORT_UDP (28780)
 
 EthernetClient client;
 
 EthernetUDP udp;
 
+//create a packetbuffer at maximum size of a udp packet
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE];
 
-char recieveBuffer[40];
+//receivebuffer for the tcp connection
+#define BUFFERLENGTH (3)
+
+char receiveBuffer[BUFFERLENGTH];
 
 //Define the pins for the fluids
-int liquid1 = 9;
-int liquid2 = 8;
-int liquid3 = 7;
-int liquid4 = 6;
-int glassPlacedPin = 5;
-int glassIndicatedPin = 4;
+#define PIN_LIQUID1 (9)
+#define PIN_LIQUID2 (8)
+#define PIN_LIQUID3 (7)
+#define PIN_LIQUID4 (6)
+#define PIN_GLASSPLACED (5)
+#define PIN_GLASSINDICATED (4)
+#define PIN_INTERRUPT (3)
 
 volatile int pulses = 0;
 volatile int chosen_liquid;
-volatile int consIter;
 
-volatile boolean glassUsed = false;
-volatile boolean glassPlaced = false;
+static int pulsesPerCL = 35;
 
+//define the states used in the state machine
 typedef enum
 {
   POURING_DRINK,
@@ -48,63 +63,83 @@ state_t current_state = NOT_CONNECTED;
 state_t next_state = NOT_CONNECTED;
 
 void setup() {
+  
+  byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xDD, 0xAF
+  };
+  //initialize the ethernet shield with DHCP
   Ethernet.begin(mac);
-
-  udp.begin(udpPort);
 
   //give the Ethernet shield a second to initialize:
   delay(1000);
 
+  //initialize the udp connection
+  udp.begin(PORT_UDP);
+
   //initialize the output pins
-  pinMode(liquid1, OUTPUT);
-  pinMode(liquid2, OUTPUT);
-  pinMode(liquid3, OUTPUT);
-  pinMode(liquid4, OUTPUT);
-  pinMode(glassPlacedPin, INPUT);
-  pinMode(glassIndicatedPin, OUTPUT);
+  pinMode(PIN_LIQUID1, OUTPUT);
+  pinMode(PIN_LIQUID2, OUTPUT);
+  pinMode(PIN_LIQUID3, OUTPUT);
+  pinMode(PIN_LIQUID4, OUTPUT);
+  pinMode(PIN_GLASSPLACED, INPUT);
+  pinMode(PIN_GLASSINDICATED, OUTPUT);
 }
 
+/*
+  This function is called when the arduino is not connected
+  to a server. It checks if there is a datagram in the buffer,
+  and if so, connect to the IP which sent it.
+*/
 boolean discoverServer() {
   int packetSize = udp.parsePacket();
   if (packetSize) {
     remote = udp.remoteIP();
     udp.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
     while (!client.connected()) {
-      client.connect(remote, tcpPort);
+      client.connect(remote, PORT_TCP);
     }
     return true;
   }
   return false;
 }
 
-boolean recieve() {
+/*
+  This function checks if there is any information available,
+  and if so, places it into the receiveBuffer.
+*/
+boolean receive() {
   if (client.available()) {
     //Read the content into a char array
     int count = 0;
     while (client.available() > 0) {
-      recieveBuffer[count] = (char)client.read();
+      char character = (char)client.read();
+      if (count < BUFFERLENGTH) {
+        receiveBuffer[count] = character;
+      }
       count++;
     }
-    //Choose what liquid to pour based on the first char
-    chooseLiquid( (char)recieveBuffer[0]);
     return true;
   }
   return false;
 }
 
+/*
+  Assignt a pin number to the variable 'chosenLiquid'
+  based on the input char.
+*/
 void chooseLiquid(char liquid) {
   switch (liquid) {
     case 'A':
-      chosen_liquid = liquid1;
+      chosen_liquid = PIN_LIQUID1;
       break;
     case 'B':
-      chosen_liquid = liquid2;
+      chosen_liquid = PIN_LIQUID2;
       break;
     case 'C':
-      chosen_liquid = liquid3;
+      chosen_liquid = PIN_LIQUID3;
       break;
     case 'D':
-      chosen_liquid = liquid4;
+      chosen_liquid = PIN_LIQUID4;
       break;
     default:
       chosen_liquid = -1;
@@ -112,39 +147,59 @@ void chooseLiquid(char liquid) {
   }
 }
 
+/*
+  This function pours the drink. It attaches an interrupt to pin 3
+  and compares the number of pulses registered to the desired amount.
+  If the glass is removed during the pouring, the pouring pauses.
+  The 'glassIndicatedPin' is connected to a LED that indicates if the
+  glass is placed.
+*/
 void pourDrink(int pin, int amount) {
-  int realAmount = ((amount * 35) - 35) / 10 ;
-  attachInterrupt(0, addPulse, RISING);
+  int realAmount = ((amount * pulsesPerCL) - pulsesPerCL) / 10 ;
+  attachInterrupt(PIN_INTERRUPT, addPulse, RISING);
   pulses = 0;
   int oldPulses = 0;
   while ( pulses < realAmount) {
-    if(digitalRead(glassPlacedPin) == HIGH){
+    if (glassPlaced()) {
       digitalWrite(pin, HIGH);
-      digitalWrite(glassIndicatedPin, HIGH);
+      digitalWrite(PIN_GLASSINDICATED, HIGH);
     } else {
       digitalWrite(pin, LOW);
-      digitalWrite(glassIndicatedPin, LOW);
+      digitalWrite(PIN_GLASSINDICATED, LOW);
     }
-    
+
   }
   digitalWrite(pin, LOW);
   detachInterrupt(0);
 }
 
+/*
+  Function that is called by the interrupt vector.
+  Simply adds a pulse to the counter.
+*/
 void addPulse() {
   pulses++;
-  consIter = 0;
 }
 
+/*
+  This function determines if there is a glass placed
+  or not and returns true or false depending on that fact.
+*/
+boolean glassPlaced() {
+  if (digitalRead(PIN_GLASSPLACED) == HIGH) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+/*
+  The main loop is basically a state machine that takes
+  action depending on what state it currently is in.
+*/
 void loop() {
   while (true) {
-    if (digitalRead(glassPlacedPin) == HIGH) {
-      glassPlaced = true;
-      digitalWrite(glassIndicatedPin, HIGH);
-    } else {
-      glassPlaced = false;
-      digitalWrite(glassIndicatedPin, LOW);
-    }
+    digitalWrite(PIN_GLASSINDICATED, glassPlaced());
     switch (current_state) {
       case NOT_CONNECTED:
         if (discoverServer()) {
@@ -152,24 +207,28 @@ void loop() {
         }
         break;
       case READY:
-        if (recieve()) {
-          if ( (char)recieveBuffer[0] == 'Q') {
+        if (receive()) {
+          //Choose what liquid to pour based on the first char
+          chooseLiquid( (char)receiveBuffer[0]);
+          
+          if ( (char)receiveBuffer[0] == 'Q') {
             client.print("OK\n");
           } else if (chosen_liquid > 0) {
             next_state = POURING_DRINK;
-          } else if ( (char)recieveBuffer[0] == 'K') {
+          } else if ( (char)receiveBuffer[0] == 'K') {
             next_state = DRINK_DONE;
           } else {
             client.print("BADFORMAT\n");
           }
         } else if (!client.connected()) {
+          client.stop();
           next_state = NOT_CONNECTED;
         }
         break;
       case POURING_DRINK:
-        if (glassPlaced) {
-          int amount = 10 * ((int)recieveBuffer[1] - 48)
-                       + ((int)recieveBuffer[2] - 48);
+        if (glassPlaced()) {
+          int amount = 10 * ((int)receiveBuffer[1] - 48)
+                       + ((int)receiveBuffer[2] - 48);
           pourDrink(chosen_liquid, amount);
           delay(100);
           //Report back to server when done
